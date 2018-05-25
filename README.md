fa7c041d2bd79efa792a10011f8be48ef3cea2e8
Роман <turbo@ippm.ru>
2018-05-25 11:31:28 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,69 @@
+# Keras inference time optimizer (KITO)
+
+This code takes on input trained Keras model and optimize layer structure and weights in such a way that model became 
+much faster, but works identically to initial model. It can be extremely useful in case you need to process large 
+amount of images with trained model. Reduce operation was tested on all Keras models zoo. See 
+comparison table below.
+ 
+## How it works?
+ 
+In current version it only apply single type of optimization: It reduces Conv2D + BatchNormalization set of layers to 
+single Conv2D layer. Since Conv2D + BatchNormalization is very common set of layers, optimization works well 
+almost on all modern CNNs for image processing.
+
+Also supported:
+* DepthwiseConv2D + BatchNormalization => DepthwiseConv2D 
+* SeparableConv2D + BatchNormalization => SeparableConv2D
+
+## How to use
+
+Typical code:
+
+```
+model.fit(...)
+...
+model.predict(...)
+```
+
+must be replaced with following block:
+
+```
+from kito import reduce_keras_model
+model.fit(...)
+...
+model_reduced = reduce_keras_model(model)
+model_reduced.predict(...)
+```
+
+So basically you need to insert 2 lines in your code to speed up operations. But note that it requires 
+some time to convert model.
+
+## Comparison table
+
+| Neural net | Input shape | Number of layers (Init) | Number of layers (Reduced) | Number of params (Init) | Number of params (Reduced) | Time to process 10000 images (Init) |  Time to process 10000 images (Reduced) | Conversion Time (sec) | Maximum diff on final layer | Average difference on final layer |  
+| --- | --- | --- |  --- |  --- |  --- |  --- |  --- |  --- |  --- |  --- |  
+| MobileNet (1.0) | (224, 224, 3) | 102 | 75 | 4,253,864| 4,221,032| 32.38 | 22.13 | 12.45 | 2.80e-06 | 4.41e-09 |
+| ResNet50 | (224, 224, 3) | 177 | 124 | 25,636,712 | 25,530,472 | 58.87 | 35.81 | 45.28 | 5.06e-07 | 1.24e-09 |
+| Inception_v3 | (299, 299, 3) | 313 | 219 | 23,851,784 | 23,817,352 | 79.15 | 59.55 | 126.02 | 7.74e-07 | 1.26e-09 |
+| Inception_Resnet_v2 | (299, 299, 3) | 782 | 578 | 55,873,736 | 55,813,192 | 131.16 | 102.38 | 766.14 | 8.04e-07 | 9.26e-10 |
+| Xception | (299, 299, 3) | 134 | 94 | 22,910,480 | 22,828,688 | 115.56 | 76.17 | 28.15 | 3.65e-07 | 9.69e-10 |
+| DenseNet121 | (224, 224, 3) | 428 | 369 | 8,062,504 | 8,040,040 | 68.25 | 57.57 | 392.24 | 4.61e-07 | 8.69e-09 |
+| DenseNet169 | (224, 224, 3) | 596 | 513 | 14,307,880 | 14,276,200 | 80.56 | 68.74 | 772.54 | 2.14e-06 | 1.79e-09 |
+| DenseNet201 | (224, 224, 3) | 708 | 609 | 20,242,984 | 20,205,160 | 98.99 | 87.04 | 1120.88 | 7.00e-07 | 1.27e-09 |
+| NasNetMobile | (224, 224, 3) | 751 | 563 | 5,326,716 | 5,272,599 | 46.05 | 31.76 | 728.96 | 1.10e-06 | 1.60e-09 |
+| NasNetLarge | (331, 331, 3) | 1021 | 761 | 88,949,818 | 88,658,596 | 445.58 | 328.16 | 1402.61 | 1.43e-07 | 5.88e-10 |
+| [ZF_UNET_224](https://github.com/ZFTurbo/ZF_UNET_224_Pretrained_Model) | (224, 224, 3) | 85 | 63 | 31,466,753 | 31,442,689 | 96.76 | 69.17 | 9.93 | 4.72e-05 | 7.54e-09 |
+| [DeepLabV3+](https://github.com/bonlime/keras-deeplab-v3-plus) (mobile) | (512, 512, 3) | 162 | 108 | 2,146,645 | 2,097,013 | 583.63 | 432.71 | 48.00 | 4.72e-05 |  |
+| [DeepLabV3+](https://github.com/bonlime/keras-deeplab-v3-plus) (xception) | (512, 512, 3) | 409 | 263 | 41,258,213 | 40,954,013 | 1000.36 | 699.24 | 333.1 | 8.63e-05 |  |
+
+Config: Single NVIDIA GTX 1080 8 GB. Timing obtained on Tensorflow backend
+
+## Notes
+
+* It feels like conversion works very slow for no reason, but it should be much faster since all 
+manipulations with layers and weights are very fast. Probably I use some very slow Keras operations in process. 
+Feel free to give advice on how to change code to make it faster.
+* You can check that both models work the same with function: compare_two_models_results(model, model_reduced, 10000)
+* Non-zero difference on final layer is accumulated because of large amount of floating point operations, which is not precise
+* Some non-standard layer or parameters (which is not used in any keras.applications CNN) can produce wrong results. 
+Most likely code will just fail in these conditions and you will see layer which cause it in python error message. 
---
+++ test_bench.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+__author__ = 'Roman Solovyev (ZFTurbo), IPPM RAS: https://github.com/ZFTurbo/'
+
+
+# Regression tests for KITO
+
+
+from kito import *
+import time
+
+
+def compare_two_models_results(m1, m2, test_number=10000, max_batch=10000):
+    i1 = m1.layers[0].input_shape
+    i2 = m2.layers[0].input_shape
+    if i1 != i2:
+        print('Different input shapes for models {} vs {}'.format(i1, i2))
+    o1 = m1.layers[-1].output_shape
+    o2 = m2.layers[-1].output_shape
+    if o1 != o2:
+        print('Different output shapes for models {} vs {}'.format(o1, o2))
+
+    t1 = 0
+    t2 = 0
+    max_error = 0
+    avg_error = 0
+    count = 0
+    for i in range(0, test_number, max_batch):
+        tst = min(test_number - i, max_batch)
+        print('Generate random images {}...'.format(tst))
+        matrix = np.random.uniform(0.0, 1.0, (tst,) + i1[1:])
+        print(matrix.shape)
+
+        start_time = time.time()
+        res1 = m1.predict(matrix)
+        t1 += time.time() - start_time
+
+        start_time = time.time()
+        res2 = m2.predict(matrix)
+        t2 += time.time() - start_time
+
+        abs_diff = np.abs(res1 - res2)
+        max_error = max(max_error, abs_diff.max())
+        avg_error += abs_diff.sum()
+        count += abs_diff.size
+
+    print("Initial model prediction time for {} random images: {:.2f} seconds".format(test_number, t1))
+    print("Reduced model prediction time for {} same random images: {:.2f} seconds".format(test_number, t2))
+    print('Models raw max difference: {} (Avg difference: {})'.format(max_error, avg_error/count))
+
+
+def get_test_neural_net(type):
+    model = None
+    if type == 'mobilenet':
+        from keras.applications.mobilenet import MobileNet
+        model = MobileNet((224, 224, 3), depth_multiplier=1, alpha=1.0, include_top=True, weights='imagenet')
+    elif type == 'resnet50':
+        from keras.applications.resnet50 import ResNet50
+        model = ResNet50(input_shape=(224, 224, 3), include_top=True, weights='imagenet')
+    elif type == 'inception_v3':
+        from keras.applications.inception_v3 import InceptionV3
+        model = InceptionV3(input_shape=(299, 299, 3), include_top=True, weights='imagenet')
+    elif type == 'inception_resnet_v2':
+        from keras.applications.inception_resnet_v2 import InceptionResNetV2
+        model = InceptionResNetV2(input_shape=(299, 299, 3), include_top=True, weights='imagenet')
+    elif type == 'xception':
+        from keras.applications.xception import Xception
+        model = Xception(input_shape=(299, 299, 3), include_top=True, weights='imagenet')
+    elif type == 'densenet121':
+        from keras.applications.densenet import DenseNet121
+        model = DenseNet121(input_shape=(224, 224, 3), include_top=True, weights='imagenet')
+    elif type == 'densenet169':
+        from keras.applications.densenet import DenseNet169
+        model = DenseNet169(input_shape=(224, 224, 3), include_top=True, weights='imagenet')
+    elif type == 'densenet201':
+        from keras.applications.densenet import DenseNet201
+        model = DenseNet201(input_shape=(224, 224, 3), include_top=True, weights='imagenet')
+    elif type == 'nasnetmobile':
+        from keras.applications.nasnet import NASNetMobile
+        model = NASNetMobile(input_shape=(224, 224, 3), include_top=True, weights='imagenet')
+    elif type == 'nasnetlarge':
+        from keras.applications.nasnet import NASNetLarge
+        model = NASNetLarge(input_shape=(331, 331, 3), include_top=True, weights='imagenet')
+    elif type == 'vgg16':
+        from keras.applications.vgg16 import VGG16
+        model = VGG16(input_shape=(224, 224, 3), include_top=False, pooling='avg', weights='imagenet')
+    elif type == 'vgg19':
+        from keras.applications.vgg19 import VGG19
+        model = VGG19(input_shape=(224, 224, 3), include_top=False, pooling='avg', weights='imagenet')
+    return model
+
+
+if __name__ == '__main__':
+    import keras.backend as K
+
+    for model_name in ['mobilenet', 'resnet50', 'inception_v3', 'inception_resnet_v2', 'xception', 'densenet121', 'densenet169', 'densenet201',
+                       'nasnetmobile', 'nasnetlarge']:
+        print('Go for: {}'.format(model_name))
+        model = get_test_neural_net(model_name)
+        print(model.summary())
+        start_time = time.time()
+        model_reduced = reduce_keras_model(model, debug=True)
+        print("Reduction time: {:.2f} seconds".format(time.time() - start_time))
+        print(model_reduced.summary())
+        print('Initial model number layers: {}'.format(len(model.layers)))
+        print('Reduced model number layers: {}'.format(len(model_reduced.layers)))
+        print('Compare models...')
+        if model_name in ['nasnetlarge', 'deeplab_v3plus_mobile', 'deeplab_v3plus_xception']:
+            compare_two_models_results(model, model_reduced, test_number=10000, max_batch=128)
+        else:
+            compare_two_models_results(model, model_reduced, test_number=10000, max_batch=10000)
+        del model
+        del model_reduced
+        K.clear_session()
