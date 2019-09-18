f94efe160e0700ed616c730dc8bd106254cd0a9d
Роман <turbo@ippm.ru>
2019-09-18 17:08:44 +0300

    1.0.3 setup

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ except ImportError:
 
 setup(
     name='kito',
-    version='1.0.0',
+    version='1.0.3',
     author='Roman Sol (ZFTurbo)',
     packages=['kito', ],
     url='https://github.com/ZFTurbo/Keras-inference-time-optimizer',
     license='MIT License',
     description='Keras inference time optimizer',
-    long_description='',
+    long_description='This code takes on input trained Keras model and optimize layer structure and weights '
+                     'in such a way that model became much faster (~10-30%), but works identically to '
+                     'initial model. It can be extremely useful in case you need to process large amount '
+                     'of images with trained model. Reduce operation was tested on all Keras models zoo. '
+                     'More details: https://github.com/ZFTurbo/Keras-inference-time-optimizer',
     install_requires=[
         'keras',
         "numpy",
